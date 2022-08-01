0a83a72e280800f795f805f4021db78315a1d5b0
pluto-0 <adamaguiar10@gmail.com>
2022-08-01 17:09:02 -0400

    added registration and got books from home page search

--- basic.py
+++ basic.py
@@ -1,35 +1,32 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm, LoginForm
-from flask_behind_proxy import FlaskBehindProxy
+#from flask_behind_proxy import FlaskBehindProxy
 from flask_sqlalchemy import SQLAlchemy
 # from api import secret_key
 import requests
 import json
 import pandas as pd
 import secrets
-import sqlalchemy as db1
+import sys
+sys.path.insert(0, 'model')
+from webscrape import Book, thriftbooks, cheapest_textbooks
+import database
 
 app = Flask(__name__)
-proxied = FlaskBehindProxy(app)
+#proxied = FlaskBehindProxy(app)
 # app.config['SECRET_KEY'] = secret_key
 sec_key = secrets.token_hex(16)
 app.config['SECRET_KEY'] = str(sec_key)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
-class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(20), unique=True, nullable=False)
-  email = db.Column(db.String(120), unique=True, nullable=False)
-  password = db.Column(db.String(60), nullable=False)
-
-  def __repr__(self):
-    return f"User('{self.username}', '{self.email}')"
+database.make_tables()
 
 
-@app.route("/", methods=['GET'])
-@app.route("/home", methods=['GET'])
+@app.route("/", methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        title = request.form['title']
+        books = cheapest_textbooks(title=title)
+        books.update(thriftbooks(title))
+        return render_template('index.html')
     return render_template('index.html')
 
 
@@ -37,10 +34,13 @@ def home():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash(f'Account created for {form.username.data}!', 'success')
+        registered = database.register(form.username.data,
+                          form.email.data,
+                          form.password.data)
+        if not registered:
+            print('An account already exists with this username/email')
+        else:
+            print(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register Here!', form=form)
 
