9f6e801740c27812318600e63597eb14277476c8
pluto-0 <adamaguiar10@gmail.com>
2022-08-02 16:46:50 -0400

    Started wishlist functionality (not working yet)

--- test.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-
-conn = sqlite3.connect('database.db')
-cursor = conn.cursor()
-command = ('SELECT * FROM wishlists')
-cursor.execute(command)
-rows = cursor.fetchall()
-print(rows)
